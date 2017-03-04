673771fcc0f50350e5f653ab5c954c54250fb6c4
Nikita P <Mansu12135@gmail.com>
2017-03-04 20:12:06 +0200

    Repair Projects

---
+++ DataBaseModel/FirstServer/FirstServer/Server.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+using CalculationModule;
+using Package;
+
+namespace FirstServer
+{
+    public class Server
+    {
+        private static ManualResetEvent AllDone = new ManualResetEvent(false);
+        private readonly BinaryFormatter Deserealizer = new BinaryFormatter();
+        private Thread ServerThread;
+
+        public static readonly IPEndPoint LocalEndPoint = new IPEndPoint(Dns.Resolve("127.0.0.1").AddressList[0], 11000);
+
+        public void StartServer()
+        {
+            ServerThread = new Thread(StartListening) { Name = "Thread for Server listening" };
+            ServerThread.Start();
+        }
+
+        public void StopServer()
+        {
+            ServerThread.Join();
+            if (ServerThread.IsAlive) { return; }
+            ServerThread.Abort();
+        }
+
+        private void StartListening()
+        {
+            var listener = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(LocalEndPoint);
+                listener.Listen(100);
+
+                while (true)
+                {
+                    AllDone.Reset();
+                    listener.BeginAccept(AcceptCallback, listener);
+                    AllDone.WaitOne();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            AllDone.Set();
+
+            var listener = (Socket)ar.AsyncState;
+            Socket handler = listener.EndAccept(ar);
+
+            var state = new State { workSocket = handler };
+            handler.BeginReceive(state.buffer, 0, State.BufferSize, 0,
+                ReadCallback, state);
+        }
+
+        private void ReadCallback(IAsyncResult ar)
+        {
+            var state = (State)ar.AsyncState;
+            Socket handler = state.workSocket;
+
+            int bytesRead = handler.EndReceive(ar);
+
+            if (bytesRead > 0)
+            {
+
+                using (var stream = new MemoryStream(state.buffer))
+                {
+                    SerializableClass newObject;
+                    try
+                    {
+                        newObject = Deserealizer.Deserialize(stream) as SerializableClass;
+                        if (newObject != null || OnDatareceived != null)
+                        {
+                            OnDatareceived.Invoke(newObject);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+                handler.BeginReceive(state.buffer, 0, State.BufferSize, 0, ReadCallback, state);
+            }
+        }
+        public delegate void ResearcherHandler(IData data);
+
+        public event ResearcherHandler OnDatareceived;
+    }
+}
---
+++ DataBaseModel/FirstServer/FirstServer/State.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+namespace FirstServer
+{
+    internal class State
+    {
+        public Socket workSocket = null;
+        public const int BufferSize = 4096;
+        public byte[] buffer = new byte[BufferSize];
+    }
+}
